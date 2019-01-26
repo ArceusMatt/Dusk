a30c57fe8105a8c7be6e8c03bdd1c0806bed4f6b
ArceusMatt <47047517+ArceusMatt@users.noreply.github.com>
2019-01-25 19:27:19 -0800

    Create Dusk-0.1.js

---
+++ Dusk-0.1.js
@@ -0,0 +1,22 @@
+var ctx = com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
+/*
+Grab current activity the launcher is running on.
+https://github.com/zhuowei/MCPELauncher/blob/master/src/com/mojang/minecraftpe/MainActivity.java
+This global variable ctx is used to run things on this activity.
+Mostly will be used to display graphical user interface(s)
+Since it seems more logical to name it MainActivity instead of ctx & ctx is a global variable, i will.
+*/
+var MainActivity = ctx;
+
+/*
+Made by ArceusMatt (c)
+copyright 2019 all rights reserved.
+
+Dusk is property of ArceusMatt & is under no open-source license...
+thus; you may not re-sell, re-distribute, or claim/copy any content without permission.
+
+Use the following link to share this software.
+https://arceusmatt.github.io/dusk.html
+*/
+
+//todo decide if the client shall be premium or open-source.
