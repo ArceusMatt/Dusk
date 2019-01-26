14d3ab0a3b8726b1c6113e0a9d9e8c97a5305e23
ArceusMatt <47047517+ArceusMatt@users.noreply.github.com>
2019-01-25 20:14:41 -0800

    Update Dusk-0.1.js

--- Dusk-0.1.js
+++ Dusk-0.1.js
@@ -20,3 +20,75 @@ https://arceusmatt.github.io/dusk.html
 */
 
 //todo decide if the client shall be premium or open-source.
+
+/*
+example:
+color.black() returns §0 //black
+(this code may change later on to randomize colors easier.)
+*/
+var color = {
+    black: function () {
+        return '§0';
+    },
+    dkBlue: function () {
+        return '§1';
+    },
+    dkGreen: function () {
+        return '§2';
+    },
+    dkAqua: function () {
+        return '§3';
+    },
+    dkRed: function () {
+        return '§4';
+    },
+    dkPurple: function () {
+        return '§5';
+    },
+    gold: function () {
+        return '§6';
+    },
+    gray: function () {
+        return '§7';
+    },
+    dkGray: function () {
+        return '§8';
+    },
+    blue: function () {
+        return '§9';
+    },
+    green: function () {
+        return '§a';
+    },
+    aqua: function () {
+        return '§b';
+    },
+    red: function () {
+        return '§c';
+    },
+    ltPurple: function () {
+        return '§d';
+    },
+    yellow: function () {
+        return '§e';
+    },
+    white: function () {
+        return '§f';
+    },
+    crypt: function () {
+        return '§k';
+    },
+    bold: function () {
+        return '§l';
+    },
+    italic: function () {
+        return '§o';
+    },
+    reset: function () {
+        return '§r';
+    },
+    rand: function () {
+        var colors = ['§1', '§2', '§3', '§4', '§5', '§6', '§7', '§8', '§9', '§0', '§a', '§b', '§c', '§d', '§e', '§f'];
+        return colors[Math.floor(Math.random() * colors.length)];
+    }
+}
